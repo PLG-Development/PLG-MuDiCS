ba24020e9060485d8b1a6226ea8df5660f159ace
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-11-06 11:07:22 +0100

    chore(nixos/update-mudics): more advanced error handling

--- nixos/update-mudics.sh
+++ nixos/update-mudics.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env nu
 
 print "Checking for new version of PLG-MuDiCS Display..."
-if (new_version_available) {
-    print "New version available. Trying to update ..."
+try {
+    if (new_version_available) {
+        print "New version available. Trying to update ..."
 
-    try {
         let temp_file_path = get_new_file
         chmod +x $temp_file_path
         mv $temp_file_path plg-mudics-display
-    } catch {
-        print "Failed to update PLG-MuDiCS Display. Starting old version."
     }
+} catch {
+    print "Failed to check or update PLG-MuDiCS Display"
 }
 
 def get_new_file [] {
     let temp_file_path = (mktemp "plg-mudics-display-XXXXXX")
-    http get https://github.com/PLG-Development/PLG-MuDiCS/releases/latest/download/plg-mudics-display | save -p -f $temp_file_path
+    http get https://github.com/PLG-Development/PLG-MuDiCS/releases/latest/download/plg-mudics-display --max-time 5sec | save -p -f $temp_file_path
     $temp_file_path
 }
 
@@ -27,7 +27,7 @@ def new_version_available [] {
 
     let current_version = open $file_path | get version
 
-    let new_version = http get https://api.github.com/repos/PLG-Development/PLG-MuDICS/releases/latest | get tag_name
+    let new_version = http get https://api.github.com/repos/PLG-Development/PLG-MuDICS/releases/latest --max-time 5sec | get tag_name
     { "version": $new_version} | to json | save version.json -f
 
     if $current_version == $new_version {
