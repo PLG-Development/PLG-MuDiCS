88b80fab585a5605764f25357bb4934836009e6a
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-11-28 16:29:50 +0100

    fix(nixos): flake not renamed to just mudics

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -8,14 +8,14 @@
     nixpkgs,
     ...
   } @ inputs: {
-    nixosConfigurations.plg-mudics = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs;
-        };
-        modules = [
-          ./configuration.nix
-        ];
+    nixosConfigurations.mudics = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {
+        inherit inputs;
       };
+      modules = [
+        ./configuration.nix
+      ];
+    };
   };
 }
