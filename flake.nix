880725a105d77e763308785d7941a2839f582006
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-12-19 13:53:58 +0100

    chore: nix dev shell support

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      packages = with pkgs; [
+        libreoffice
+        ungoogled-chromium
+        xreader
+        imagemagick
+        ffmpeg
+        ghostscript
+        gnome-screenshot
+      ];
+    };
+  };
+}
